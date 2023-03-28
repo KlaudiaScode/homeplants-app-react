208c614b6b3d04eeb1bce2dfa6db180d8c30de6e
Klaudia Sagan <klaudiasagan.code@gmail.com>
2023-03-28 19:13:34 +0200

    created layout for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import plant from './img/plant.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="houseplants">
+        <div className='header'>
+          <div className='logo'>
+            <img src={plant} alt="Plant with pot" />
+          </div>
+          <h1>Hello User👋</h1>
+          <div className='dropdown'>
+            <button className="menu" 
+              data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="10,20">
+              𝄙
+            </button>
+            <ul className="dropdown-content">
+              <li><a className="active" href="#home">Home</a></li>
+              <li><a href="#my_profile">My profile</a></li>
+              <li> <a href="#my_plants">My plants</a></li>
+              <li><a href="#plants_mail">Plants mail</a></li>
+              <li><a href="#sale_of_plants">Sale of plants</a></li>
+              <li><a href="#green_group">Green group</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className='search'>
+              <input type='text' placeholder='Search new plant...'></input>
+              <button type='submit'>📸</button>
+              <button type='submit'>🔎</button>
+        </div>
+        <div className='main'>
+          <div className='calendar'>TUTAJ BĘDZIE KALENDARZ UŻYTKOWNIKA</div>
+          <div className='plant_class'>
+              <p>
+                <b>Monstera Deliciosa</b> uwielbia mocno wilgotne powietrze,
+                warto często ją zraszać przy użyciu atomizera, 
+                zwłaszcza w sezonie grzewczym. Monstera nie wymaga 
+                częstego podlewania, zwykle raz w tygodniu latem,
+                a zimą dwa razy w miesiącu.<a href='#plant_class'>⮕</a>
+              </p>
+          </div>
+        </div>
+        <div className='advertisement'>
+          <a href='#advertisement'><p>Miejsce na Twoją reklamę</p></a>
+        </div>
+        <div className='icons'>
+          <button>🏠</button>
+          <button>👤</button>
+          <button>🌿</button>
+          <button>💰</button>
+          <button>👥</button>
+          <button>💬</button>
+          <button>💡</button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
+
